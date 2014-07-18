97c6396ecdaf8958a3063ec8c234b02a019fd471
Jim Kang <jimkang@gmail.com>
2014-07-17 23:31:32 -0400

    Added falsePositives check to iscool.js.

--- iscool.js
+++ iscool.js
@@ -2,10 +2,19 @@ var behavior = require('./behaviorsettings');
 var logger = require('./logger');
 
 function isCool(word) {
-  var cool = (behavior.buzzkillBlacklist.indexOf(word) === -1);
+  var cool = (behavior.falsePositivesList.indexOf(word) === -1);
+  if (!cool) {
+    return cool;
+  }
+
+  if (cool) {
+    cool = (behavior.buzzkillBlacklist.indexOf(word) === -1);
+  }
+
   if (cool && behavior.tragedyHappenedRecently) {
     cool = (behavior.tragedyModeBlacklist.indexOf(word) === -1);
   }
+
   if (!cool) {
     logger.log('Uncool word', word);
   }
