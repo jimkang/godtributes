1f5054cd55df4f33c03cfd31d641d2cc1b86e0d7
Jim Kang <jimkang@gmail.com>
2014-07-18 23:45:42 -0400

    Added to prep phrase table.

--- prepphrasepicker.js
+++ prepphrasepicker.js
@@ -2,8 +2,9 @@ var probable = require('probable');
 
 var prepPhraseTable = probable.createRangeTableFromDict({
   'FOR THE': 75,
-  'TO THE': 20,
-  'BEFORE THE': 5
+  'TO THE': 4,
+  'UNTO THE': 15,
+  'BEFORE THE': 1
 });
 
 function getPrepPhrase() {
