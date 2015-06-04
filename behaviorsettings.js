5a489b6a4d97ed5ebfe92fcbb9f137d64b59de2c
Jim Kang <jimkang@gmail.com>
2015-06-04 09:54:21 -0400

    Inched up hoursToWaitBetweenRepliesToSameUser.

--- behaviorsettings.js
+++ behaviorsettings.js
@@ -1,6 +1,6 @@
 module.exports = {
   exhortTestSubjectUserId: 129586119,
-  hoursToWaitBetweenRepliesToSameUser: 42.5,
+  hoursToWaitBetweenRepliesToSameUser: 52,
   maxAttemptsToReplyPerUserPerRun: 1,
   tragedyHappenedRecently: true,
   maxCommonnessForReplyTopic: [20, 40],
