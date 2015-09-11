e5f68c013db3a40e0acdef6f24c20117a8b9dfc3
Jim Kang <jimkang@gmail.com>
2015-09-11 13:31:01 -0400

    Updated mutualize-follower-list to pass a blacklist to filteredFollowback (to reduce the number of API calls) and to use filteredFollowback's callback to update the blacklist.

--- mutualize-follower-list.js
+++ mutualize-follower-list.js
@@ -1,20 +1,33 @@
 var filteredFollowback = require('filtered-followback');
 var config = require('./config.js');
+var jsonfile = require('jsonfile');
+var _ = require('lodash');
+
+var spamListLocation = __dirname + '/data/spam-users.json';
+var spamUsers = jsonfile.readFileSync(spamListLocation);
 
 filteredFollowback(
   {
     twitterCreds: config.twitter,
     neverUnfollow: [
       2205976656
-    ]
+    ],
+    blacklist: spamUsers
   },
   reportResults
 );
 
-function reportResults(error, followed, unfollowed) {
+function reportResults(error, followed, unfollowed, filteredOut) {
   if (error) {
     console.log(error);
   }
   console.log('Followed:', followed);
   console.log('Unfollowed:', unfollowed);
+  console.log('Filtered out:', filteredOut);
+  updateSpamList(filteredOut);
+}
+
+function updateSpamList(filteredOut) {
+  var newSpamList = _.union(spamUsers, filteredOut);
+  jsonfile.writeFileSync(spamListLocation, newSpamList);
 }
