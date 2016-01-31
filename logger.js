0158c243b8263bd458cde57e201a2d0297dfe536
Jim Kang <jimkang@gmail.com>
2016-01-31 00:49:42 -0500

    Yo-yo back to fancy log formatting.

--- logger.js
+++ logger.js
@@ -18,4 +18,38 @@ var sysLog = bunyan.createLogger({
   ]
 });
 
+// Console.log style formatting.
+function formatMessage() {
+  var params = Array.prototype.slice.call(arguments);
+  var formatted = '';
+  for (var i = 0; i < arguments.length; ++i) {
+    if (formatted.length > 0) {
+      formatted += ' ';
+    }
+    var arg = arguments[i];
+    if (typeof arg === 'string') {
+      formatted += arg;
+    }
+    else if (typeof arg === 'object') {
+      formatted += JSON.stringify(arg);
+    }
+    else {
+      formatted += arg.toString();
+    }
+  }
+  return formatted;
+}
+
+function info() {
+  sysLog.info(formatMessage(arguments));
+}
+
+function warn() {
+  sysLog.warn(formatMessage(arguments));
+}
+
+function error() {
+  sysLog.error(formatMessage(arguments));
+}
+
 module.exports = sysLog;
