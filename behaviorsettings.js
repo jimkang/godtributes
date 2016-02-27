302c9b26e103c82b7ce729bfc7c2e0641c456726
Jim Kang <jimkang@gmail.com>
2016-02-26 23:23:48 -0500

    Loosen up text noun selection slightly; tighten up image noun selection.

--- behaviorsettings.js
+++ behaviorsettings.js
@@ -3,8 +3,8 @@ module.exports = {
   hoursToWaitBetweenRepliesToSameUser: 52,
   maxAttemptsToReplyPerUserPerRun: 1,
   tragedyHappenedRecently: true,
-  maxCommonnessForReplyTopic: [20, 40],
-  maxCommonnessForSecondaryTopic: [50, 90],
-  maxCommonnessForImageTopic: [1000, 10000],
+  maxCommonnessForReplyTopic: [20, 70],
+  maxCommonnessForSecondaryTopic: [50, 100],
+  maxCommonnessForImageTopic: [500, 5000],
   emojiThresholdPercentage: 5
 };
