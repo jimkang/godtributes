10dca61bcac911d794a8f8c04a33fed8e2f4064b
Jim Kang <jimkang@gmail.com>
2018-01-31 08:54:31 -0500

    Add footer to archive.

--- maketribute.js
+++ maketribute.js
@@ -26,7 +26,12 @@ var randomId = require('idmaker').randomId;
 var staticWebStream = StaticWebArchiveOnGit({
   config: config.github,
   title: config.archiveName,
-  footerScript: `<script type="text/javascript">
+  footerHTML: `<footer>
+  Banner by <a href="https://twitter.com/metroidbaby">@metroidbaby</a>.
+  Powered by <a href="http://developer.wordnik.com/">Wordnik</a>.
+  On Twitter: <a href="https://twitter.com/godtributes">@godtributes</a>.
+</footer>
+<script type="text/javascript">
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
