274dca6b4aa0d25ad3e52e9597a9992714cc5b0a
Jim Kang <jimkang@gmail.com>
2018-06-05 23:38:08 -0400

    Add do not respond integration test.

--- tests/integration/exhort-tweet-test.js
+++ tests/integration/exhort-tweet-test.js
@@ -1,6 +1,4 @@
-/* global process */
-
-var assert = require('assert');
+var test = require('tape');
 var createExhorter = require('../../exhorter');
 var tributeDemander = require('../../tributedemander');
 var Chronicler = require('basicset-chronicler').createChronicler;
@@ -43,19 +41,48 @@ var targetTweet = {
   },
   text: "Trader Joe's has cheap persimmons again! https://flic.kr/p/qcTcz2"
 };
+var targetTweet2 = {
+  user: {
+    id: 546402627261833217,
+    id_str: '546402627261833217',
+    screen_name: 'deathmtn'
+  },
+  text:
+    'The news about Kate Spade is dreadful. She left a 13-year old daughter behind! Her husband Andy’s stepfather committed suicide when he and his brothers (Bryan & David- yes, that one) were still kids, too. So much awfulness.'
+};
 
-// Make sure the chronicler server is running before you run this.
+var testCases = [
+  {
+    name: 'Respond to tweet',
+    shouldRespond: true,
+    tweet: targetTweet
+  },
+  {
+    name: 'Do not respond to tweet',
+    shouldRespond: false,
+    tweet: targetTweet2
+  }
+];
 
-exhorter.getExhortationForTweet(targetTweet, function checkResult(
-  error,
-  tweet,
-  exhortation,
-  topics
-) {
-  console.log('error:', error);
-  assert.ok(!error);
-  assert.ok(exhortation);
-  assert.ok(Array.isArray(topics));
-  console.log(exhortation);
-  process.exit();
-});
+testCases.forEach(runTest);
+
+function runTest(testCase) {
+  test(testCase.name, exhortationTest);
+
+  function exhortationTest(t) {
+    exhorter.getExhortationForTweet(testCase.tweet, checkResult);
+
+    function checkResult(error, tweet, exhortation, topics) {
+      console.log('error:', error);
+      if (testCase.shouldRespond) {
+        t.ok(!error);
+        t.ok(exhortation);
+        t.ok(Array.isArray(topics));
+        console.log(exhortation);
+      } else {
+        t.ok(error);
+      }
+      t.end();
+    }
+  }
+}
